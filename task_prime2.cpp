240af8f9db9985682ceca5bfa53108b57e7eae42
kamalatha99 <69569790+kamalatha99@users.noreply.github.com>
2021-05-12 20:39:02 +0530

    Add files via upload

---
+++ task_prime2.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n,i,fc=0;
+	cin>>n;
+	for(i=2;i<=n/2;i++)
+	{
+		if (n%i==0)
+		{
+			fc++;
+        }
+	}
+	cout<<"factors="<<fc<<endl;
+	if(fc==0)
+	{
+		cout<<"prime";
+	}
+	else
+	{
+		cout<<"not a prime";
+	}
+	return 0;
+}
