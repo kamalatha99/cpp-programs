aa16f314f07f81e5f8c3e4fc66cd2e8a760eecf6
kamalatha99 <69569790+kamalatha99@users.noreply.github.com>
2021-06-08 10:47:55 +0530

    Add files via upload

---
+++ functions.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+ //functions void type && return type
+/* Return type returns a value to the  main() function */
+
+/*void type doesnt returns anything to main function  */
+void add(int a,int b)
+{
+	cout<<a+b;
+}
+int main()
+{
+	int a=10,b=9;
+	add(a,b);
+}
+
